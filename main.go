f1d04c362d9e24954b124aa808065c8843bd1dbe
James Cohen <jamescohen@Jamess-MacBook-Air.local>
2024-05-04 08:19:19 -0500

    added dynamic port

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -11,6 +12,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port if PORT environment variable is not set
+	}
+
+	portString := ":" + port
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(portString, nil))
 }
