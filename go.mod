4db9ceaac625b639497e46fa7d0780850d9b33e0
James Cohen <jamescohen@Jamess-MacBook-Air.local>
2024-05-04 07:50:49 -0500

    remove build removal lol

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jaycoh/jaymes.co.uk
 
 go 1.21.1
+
+
--- main.go
+++ main.go
@@ -1,18 +1,16 @@
-//go:build ignore
-
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
